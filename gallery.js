1b5a46439211f0d6ff602c7fa178dcf93ea621b1
Kayla Paden <kaylamarie1785@gmail.com>
2025-02-05 20:28:59 -0600

    Dynamically add main carousel slides

--- gallery.js
+++ gallery.js
@@ -1,43 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
 
-  const splide = new Splide( '#main-carousel', {
-    type: "slide",
-    width: "90%",
-    perPage: 1,
-    perMove: 1,
-    gap: "100px",
-    padding: "10%",
-    rewind: true,
-    pagination: false,
-    arrows: true,
-  } );
-  
-  const thumbnails = document.getElementsByClassName( 'thumbnail' );
-  let current;
-  
-  for ( let i = 0; i < thumbnails.length; i++ ) {
-    initThumbnail( thumbnails[ i ], i );
-  }
-  
-  function initThumbnail( thumbnail, index ) {
-    thumbnail.addEventListener( 'click', function () {
-      splide.go( index );
-    } );
-  }
-  
-  splide.on( 'mounted move', function () {
-    const thumbnail = thumbnails[ splide.index ];
-  
-    if ( thumbnail ) {
-      if ( current ) {
-        current.classList.remove( 'is-active' );
-      }
-  
-      thumbnail.classList.add( 'is-active' );
-      current = thumbnail;
-    }
-  } );
-  
-  splide.mount();
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const json = "/gallery.json";
+
+  fetch(json)
+  .then(response => response.json())
+  .then(data => renderGallery(data))
+  .catch(error => console.error("Error loading gallery data: ", error));
 });
+
+function renderGallery(data) {
+  const gallery = document.querySelector('.splide__list');
+
+  data.gallery.forEach(item => {
+    const slide = document.createElement('li');
+
+    slide.classList.add('splide__slide');
+
+ slide.innerHTML = `
+      <img src="${item.imageUrl}" alt="${item.description}" />
+      <div class="slide-description">
+      <cite>${item.title}</cite>
+      <q>${item.description}</q>
+        <p>${item.artist}</p>
+      </div>
+    `;
+    gallery.appendChild(slide);
+  });
+}
