5524402eb3063afb1fb8115f22d496e9b63666dc
Kayla Paden <kaylamarie1785@gmail.com>
2025-02-05 20:53:03 -0600

    Dynamically create thumbnails and styled frame around active gallery item

--- gallery.js
+++ gallery.js
@@ -10,20 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function renderGallery(data) {
   const gallery = document.querySelector('.splide__list');
+  const thumbnails = document.querySelector('#thumbnails');
 
   data.gallery.forEach(item => {
     const slide = document.createElement('li');
+    const thumbnail = document.createElement('li');
 
     slide.classList.add('splide__slide');
+    thumbnail.classList.add('thumbnail');
 
  slide.innerHTML = `
       <img src="${item.imageUrl}" alt="${item.description}" />
       <div class="slide-description">
       <cite>${item.title}</cite>
       <q>${item.description}</q>
-        <p>${item.artist}</p>
+        <p>- ${item.artist}</p>
       </div>
     `;
+
+    thumbnail.innerHTML = `
+      <img src="${item.imageUrl}" alt="${item.description}" />
+    `;
+
     gallery.appendChild(slide);
+    thumbnails.appendChild(thumbnail);
   });
 }
