723cbef51b33fea1919dc877f4859576af1dd5ad
Kayla Paden <kaylamarie1785@gmail.com>
2025-02-06 22:36:28 -0600

    Fix splide content rendering timing

--- gallery.js
+++ gallery.js
@@ -1,11 +1,15 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-  const json = "/gallery.json";
+document.addEventListener("DOMContentLoaded", async function () {
 
-  fetch(json)
-  .then(response => response.json())
-  .then(data => renderGallery(data))
-  .catch(error => console.error("Error loading gallery data: ", error));
+  try {
+  const json = "/gallery.json";
+  const response = await fetch(json);
+  const data = await response.json();
+  renderGallery(data);
+  initializeSplide();
+} catch (error) {
+  console.error("Error loading gallery data: ", error);
+}
 });
 
 function renderGallery(data) {
@@ -35,4 +39,47 @@ function renderGallery(data) {
     gallery.appendChild(slide);
     thumbnails.appendChild(thumbnail);
   });
-}
+};
+
+function initializeSplide() {
+
+  const splide = new Splide( '#main-carousel', {
+    type: "slide",
+    width: "90%",
+    perPage: 1,
+    perMove: 1,
+    gap: "100px",
+    padding: "10%",
+    rewind: true,
+    pagination: false,
+    arrows: true,
+  } );
+  
+  const thumbnails = document.getElementsByClassName( 'thumbnail' );
+  let current;
+  
+  for ( let i = 0; i < thumbnails.length; i++ ) {
+    initThumbnail( thumbnails[ i ], i );
+  }
+  
+  function initThumbnail( thumbnail, index ) {
+    thumbnail.addEventListener( 'click', function () {
+      splide.go( index );
+    } );
+  }
+  
+  splide.on( 'mounted move', function () {
+    const thumbnail = thumbnails[ splide.index ];
+  
+    if ( thumbnail ) {
+      if ( current ) {
+        current.classList.remove( 'is-active' );
+      }
+  
+      thumbnail.classList.add( 'is-active' );
+      current = thumbnail;
+    }
+  } );
+  
+  splide.mount();
+};
